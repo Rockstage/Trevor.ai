08845b05de344856f6b8366d251f3b32d8b7067f
George Petrov <jokopetrov@gmail.com>
2016-11-09 16:33:08 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'press' => 'home#press'
   get 'team' => 'home#team'
   get 'fi' => 'home#fi'
+  get 'founderinstitute' => 'home#fi'
+  get 'founder-institute' => 'home#fi'
+  get 'foundersinstitute' => 'home#fi'
+  get 'founders-institute' => 'home#fi'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
